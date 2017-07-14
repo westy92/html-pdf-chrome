2ea21dfdede547e2342f22d15cca891d88ba13f0
Seth Westphal <westy92@gmail.com>
2017-07-13 23:35:17 -0500

    Specify explit coverage file.

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('coverage', ['test'], () => {
         'text': null,
         'text-summary': null,
         'html': 'coverage/html-report',
-        'json': 'coverage/coverage-final.json',
+        'json': 'coverage/coverage-remapped.json',
       }
     }));
 });
